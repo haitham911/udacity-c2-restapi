c9cac80a4bc1f819941acb8202dabfede31b571b
haitham911 <haitham911eg@gmail.com>
2022-07-24 04:37:13 +0200

    fix cors

--- src/server.ts
+++ src/server.ts
@@ -24,8 +24,9 @@ await datasource
 
   //CORS Should be restricted
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    
     next();
   });
 
@@ -43,3 +44,5 @@ await datasource
       console.log( `press CTRL+C to stop server` );
   } );
 })();
+
+
